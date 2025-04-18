65bcba972d847ff8b86f72737bfbfce535c073fb
DevGuedes <guedesindev@gmail.com>
2025-04-17 23:18:03 -0300

    FirebaseConfig sem os dados pessoais

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -19,13 +19,7 @@ import {
 } from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBKFqcmWVEqO1rvQhSJsCc4nAC6fDfCYW8',
-  authDomain: 'tic-tac-toe-7dc79.firebaseapp.com',
-  databaseURL: 'https://tic-tac-toe-7dc79-default-rtdb.firebaseio.com',
-  projectId: 'tic-tac-toe-7dc79',
-  storageBucket: 'tic-tac-toe-7dc79.firebasestorage.app',
-  messagingSenderId: '1000903973824',
-  appId: '1:1000903973824:web:0a8922fc30f29c89f00625'
+  //cole aqui as configurações da tua aplicação firebase
 }
 
 initializeApp(firebaseConfig)
